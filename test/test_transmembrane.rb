484900e6c582829da8bfa2fac9dcc9deb90ce725
Ben J. Woodcroft <donttrustben@gmail.com>
2011-03-24 23:40:40 +1100

    initial import of code from the netherworld, no worky yet

--- test/test_transmembrane.rb
+++ test/test_transmembrane.rb
@@ -1,12 +1,4 @@
-# 
-# To change this template, choose Tools | Templates
-# and open the template in the editor.
- 
-
-$:.unshift File.join(File.dirname(__FILE__),'..','lib')
-
-require 'test/unit'
-require 'transmembrane'
+require 'helper'
 
 module Transmembrane
   class TransmembraneTest < Test::Unit::TestCase
